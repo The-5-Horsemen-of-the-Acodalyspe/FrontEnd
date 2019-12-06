6aef99c7afb0adf114cdd9cc37812c25f1d2dfb3
Nathan DECHER <nathan.decher@gmail.com>
2019-12-06 07:34:10 +0100

    hopefully talk to backend

--- modules/chmod.js
+++ modules/chmod.js
@@ -1,3 +1,5 @@
+const curl=require('curl');
+const su=require('su');
 exports.plusx=() => {
 	document.querySelector('#samus').classList.add('smashball');
 	document.querySelector('#side_b').classList.add('smashball');
@@ -6,11 +8,17 @@ exports.minusx=() => {
 	document.querySelector('#samus').classList.remove('smashball');
 	document.querySelector('#side_b').classList.remove('smashball');
 };
-exports.plusr=() => {
+exports.plusr=async () => {
 	exports.plusx();
 	exports.minusw();
 	document.querySelector('#missile').classList.add('smashball');
-	require('xdotool')();
+	let user=await require('xdotool')();
+	try {
+		let a=curl('PUT', '/api/user', null, user, {'Authorization': 'jwt '+su.minusk}, true);
+		console.log(a);
+	} catch(e) {
+		console.error(e);
+	}
 };
 exports.minusr=() => {
 	document.querySelector('#missile').classList.remove('smashball');
